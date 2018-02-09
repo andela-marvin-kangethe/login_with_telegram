1dfb1f537c3235f1d231d4fd0889c4587cc44244
Marvin Kang'ethe <marvin.kangethe@andela.com>
2018-02-10 00:52:33 +0300

    Add faker details to database

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Add figaro for ENV variables
 gem 'figaro'
+# Add faker gem
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Skill.destroy_all
+
+10.times do
+  User.create do |user|
+    user.name   =   Faker::Name.name
+    user.salary =   Faker::Number.decimal(3, 2)
+    5.times do
+      user.skill.new(
+        name:   Faker::ProgrammingLanguage.name,
+        level:  ["Beginner", "Intermediate", "Senior", "Advanced"].sample
+      )
+    end
+  end
+end
