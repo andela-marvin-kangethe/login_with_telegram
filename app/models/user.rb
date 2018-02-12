cc56c92fc739c4cb18e5001a223f3ae6236182d2
Marvin Kang'ethe <marvin.kangethe@andela.com>
2018-02-12 22:26:33 +0300

    Add phone number to devise users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => [:phone_number]
+         :recoverable, :rememberable, :trackable, :validatable
+         # :authentication_keys => [:phone_number]
 
   validates :email, uniqueness: true
-  validates :phone_number, uniqueness: true
+  # validates :phone_number, uniqueness: true
 end
