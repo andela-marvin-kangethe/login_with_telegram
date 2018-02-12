5c63e822289d279d62905adb5ddc750b6e51b8b3
Marvin Kang'ethe <marvin.kangethe@andela.com>
2018-02-12 22:08:02 +0300

    Add devise user

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:phone_number]
+
+  validates :email, uniqueness: true
+  validates :phone_number, uniqueness: true
+end
